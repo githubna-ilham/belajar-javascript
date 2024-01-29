0cb358d9b5492dff50f83ade6b33a1099aecb12b
githubna-ilham <ilham.anugrah@unpas.ac.id>
2024-01-29 14:04:41 +0700

    pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,10 @@
+var x = prompt("Masukan Angka :");
+// alert("selamat datang " + nama);
+
+if (x % 2 == 0) {
+  alert(x + " bilangan genap");
+} else if (x % 2 == 1) {
+  alert(x + " bilangan ganjil");
+} else {
+  alert("data bukan angka");
+}
