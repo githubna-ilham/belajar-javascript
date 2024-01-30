e3d3220100e812cbfc7a4960fb922b3033166a02
githubna-ilham <ilham.anugrah@unpas.ac.id>
2024-01-30 08:49:54 +0700

    object json

--- javascript-dom/object.js
+++ javascript-dom/object.js
@@ -1,3 +1,4 @@
+//object
 var siswa = new Object();
 siswa.namaDepan = "agung";
 siswa.namaBelakang = "alfatah";
@@ -6,7 +7,18 @@ siswa.namaLengkap = function () {
   return this.namaDepan + " " + this.namaBelakang;
 };
 
+//object JSON
+var siswa2 = {
+  namaDepan: "ilham",
+  namaBelakang: "anugrah",
+  alamat: "bandung",
+  namaLengkap: function () {
+    return this.namaDepan + " " + this.namaBelakang;
+  },
+};
+
 alert("Nama :" + siswa.namaLengkap());
+alert("Nama : " + siswa2.namaLengkap());
 
 // function namaLengkap() {
 
